4b467cf29c1f8667b0081be247736e35fdb4816f
pwian <pwian@mail.ru>
2020-02-28 19:15:23 +0300

    Add test

---
+++ ExcelTest.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Excel
+{
+    [TestFixture]
+    public class ExcelTest
+    {
+        [Test]
+        public void ReadWriteTest()
+        {
+            var inputString = "TestValue";
+            string readString;
+            var fileName = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestExcel.xlsx");
+            using (var excel = new Excel(fileName))
+            {
+                excel.WriteToCell(1, 1, inputString);
+                readString = excel.ReadCell(1, 1);
+                excel.Save();
+                excel.Close();
+            }
+
+            Assert.AreEqual(inputString, readString);
+        }
+    }
+}
